36fbaaf8bc5c6744dad97062cf7077549b24a738
MuhammadVT <mamatripik@gmail.com>
2018-06-11 19:28:54 -0400

    seperate a file into two

--- data_preprocessing/fill_missing_tec_map.py
+++ data_preprocessing/fill_missing_tec_map.py
@@ -4,38 +4,6 @@ import sqlite3
 import datetime as dt
 import os
 
-def create_tec_map_table(sdate, edate, tec_resolution=5,
-                         file_dir="../data/tec_map/filled/",
-                         table_name="tec_map_filled", 
-                         db_name="tec_map.sqlite", 
-                         db_dir="../data/sqlite3/"):
-    """Creats a table in SQLite db to store datetimes of tec maps
-       and their file paths"""
-
-    # Make a db connection
-    conn = sqlite3.connect(db_dir + db_name)
-
-    # Create a table
-    schema = "Create Table IF NOT EXISTS {tbl} (" +\
-	     "datetime TIMESTAMP, "+\
-             "file_path TEXT, " +\
-             "PRIMARY KEY datetime)"
-    schema = schema.format(tbl=table_name)
-
-    # Create a dataframe
-    edate = edate + dt.timedelta(days=1)    # Make the end date inclusive
-    nmaps = int(round((edate - sdate).total_seconds() / 60. / tec_resolution))
-    dtms = [sdate + dt.timedelta(minutes=tec_resolution*i) for i in range(nmaps)] 
-    files_all = [file_dir + dtm.strftime("%Y%m%d") + "/" +\
-                 dtm.strftime("%Y%m%d.%H%M") + ".npy" for dtm in dtms]
-    files = [f.replace(file_dir, "") if os.path.isfile(f) else "NaN" for f in files_all]
-    df = pd.DataFrame(data={"datetime":dtms, "file_path":files})
-
-    # Write data to db
-    df.to_sql(table_name, conn, schema=schema, if_exists="append", index=False)
-
-    return
-
 def fill_missing_tec_map(tec_resolution=5, nframes_before=3, nframes_after=3,
                          table_name="tec_map_filled", 
                          db_name="tec_map.sqlite", 
@@ -55,7 +23,6 @@ def fill_missing_tec_map(tec_resolution=5, nframes_before=3, nframes_after=3,
     cur.execute(command)
     rows = cur.fetchall()
     dtms = [x[0] for x in rows]
-    print("There are {nframes} frames missing".format(nframes=len(dtms)))
 
     # Fill the missing frames one at a time
     for i, dtm in enumerate(dtms):
@@ -95,7 +62,7 @@ def fill_missing_tec_map(tec_resolution=5, nframes_before=3, nframes_after=3,
                 command = command.format(tbl=table_name, fname=fname_i.replace(file_dir, ""),
                                          dtm = dtm)
                 cur.execute(command)
-                print("Filled the missing frame for " + str(dtm))
+                #print("Filled the missing frame for " + str(dtm))
             else:
                 continue
 
@@ -105,6 +72,8 @@ def fill_missing_tec_map(tec_resolution=5, nframes_before=3, nframes_after=3,
     # Close db connection
     conn.close()
 
+    print("Filled {nframes} missing frames in total".format(nframes=len(dtms)))
+
     return
 
 
@@ -113,16 +82,9 @@ if __name__ == "__main__":
 
     # initialize parameters
     sdate = dt.datetime(2015, 1, 1)
-    edate = dt.datetime(2015, 1, 17)    # Includes the edate
+    edate = dt.datetime(2015, 3, 1)    # Includes the edate
     tec_resolution = 5
     file_dir="../data/tec_map/filled/"
-
-    # Create a table for storing tec map datetimes and file paths
-    create_tec_map_table(sdate, edate, tec_resolution=tec_resolution,
-                         file_dir=file_dir,
-                         table_name="tec_map_filled", 
-                         db_name="tec_map.sqlite", 
-                         db_dir="../data/sqlite3/")
 
     # Fill the missing frames with adjacent frames
     fill_missing_tec_map(tec_resolution=tec_resolution,
