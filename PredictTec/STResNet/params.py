e3bce3f2d132f55cbc8bd83ec85082be89316cc5
Sneha Singhania <sneha.a@iiitb.org>
2018-07-01 15:27:36 +0530

    params.py updated for exogenous

--- PredictTec/STResNet/params.py
+++ PredictTec/STResNet/params.py
@@ -12,12 +12,19 @@ class Params(object):
     period_sequence_length = 24
     trend_sequence_length =  8
     num_of_filters = 64
-    num_of_residual_units = 3 #increase this value during the actual training
+    num_of_residual_units = 5 #increase this value during the actual training
     num_of_output_tec_maps = 1
+    look_back = 288
+    exo_values = 4 #by, bz, vx, np
+    lstm_size = 64
+    num_layers = 3
     epsilon = 1e-7
     beta1 = 0.8
     beta2 = 0.999
     lr = 0.001
-    num_epochs = 10
-    num_samples = 500
+    num_epochs = 20
+    delta = 0.5
+    model_path = './model'
     logdir = "train"
+    
+
