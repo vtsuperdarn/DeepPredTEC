363c2a4729c9267999c63945ab11c26024a37163
MuhammadVT <mamatripik@gmail.com>
2018-09-16 11:47:06 -0400

    plot err by hour

--- ModelValidation/pred_err_by_hour.py
+++ ModelValidation/pred_err_by_hour.py
@@ -13,11 +13,18 @@ def load_tec(file_path):
     x = np.load(file_path)
     return x
 
-def get_tec(stime, etime, pred_tec_dir, time_window=[0, 1], pred_time_step=10,
+def get_tec(stime, etime, pred_tec_dir, time_window=[0, 1],
+            pred_time_step=10, base_model="previous_day",
             filled_tec_dir = "../data/tec_map/filled/"):
 
     """ Reads predicted and true TEC maps for a given time_windown for time 
-    interval between stime and etime"""
+    interval between stime and etime
+
+    Parameters
+    ----------
+    base_model : str
+        The name of the baseline models, valid inputs are "static", "previous_day"
+    """
 
     # Set the stime minute to 0
     stime = stime.replace(minute=0)
@@ -27,55 +34,49 @@ def get_tec(stime, etime, pred_tec_dir, time_window=[0, 1], pred_time_step=10,
     dtms = [stime + dt.timedelta(minutes=i*pred_time_step) for i in range(num_dtms)]
 
     # Select time tags within time_window
-    dtms = [x for x in dtms if ((x.hour>=time_window[0]) & (x.hour<time_window[1]))]
+    dtms = [x for x in dtms if (((60*x.hour+x.minute)>60*time_window[0]) &\
+                                ((60*x.hour+x.minute)<=60*time_window[1]))]
     
     # Get corresponding file names
     pred_tec_files = [os.path.join(pred_tec_dir, x.strftime("%Y%m%d.%H%M") + "_pred.npy")\
                       for x in dtms]
-    true_tec_files = [os.path.join(filled_tec_dir, x.strftime("%Y%m%d.%H%M") + ".npy")\
-                      for x in dtms]
+    true_tec_files = [os.path.join(filled_tec_dir, x.strftime("%Y%m%d"),\
+                                   x.strftime("%Y%m%d.%H%M") + ".npy") for x in dtms]
+
+    if base_model is None:
+        base_tec_files = None
+    elif base_model == "previous_day":
+        base_tec_files = [os.path.join(filled_tec_dir, (x-dt.timedelta(days=1)).strftime("%Y%m%d"),\
+                         (x-dt.timedelta(days=1)).strftime("%Y%m%d.%H%M") + ".npy") for x in dtms]
+    elif base_model == "static":
+        base_tec_files = None
 
     # Read the True TEC maps
-    pred_tec = [load_tec(x).compute() for x in pred_tec_files]
+    pred_tec = np.array([load_tec(x).compute() for x in pred_tec_files])
+    true_tec = np.array([load_tec(x).compute() for x in true_tec_files])
+    if base_tec_files is not None:
+        base_tec = np.array([load_tec(x).compute() for x in base_tec_files])
+    else:
+        base_tec  = None
 
+    return true_tec, pred_tec, base_tec
 
-    return true_tec, pred_tec
-
-def calc_avg_tec(stime, etime, time_window, pred_tec_dir, error_type="absolute",
-                 base_model="static", pred_time_step=10,
-                 filled_tec_dir = "../data/tec_map/filled/"):
-    """ Calculates the average predicted TEC error for each time_window for 
-    the time interval between stime and etime
-    
-    Parameters
-    ----------
-    base_model : str
-        The name of the baseline models, valid inputs are "static", "previous_day"
-    error_type : str
-        The type of error values
-
+def calc_avg_err(true_tec, pred_tec):
+    """ Calculates the average predicted TEC error
     """
 
+    err_dict = {}
+    pred_true_diff =  np.abs(pred_tec - true_tec)
+    abs_avg_err = pred_true_diff.mean(axis=0)
+    err_dict["pred_true_diff"] = pred_true_diff
+    err_dict["Average Absolute Error"] = abs_avg_err
+    err_dict["abs_avg_err_std"] = pred_true_diff.std(axis=0)
+    err_dict["abs_avg_err_max"] = pred_true_diff.max(axis=0)
+    err_dict["abs_avg_err_min"] = pred_true_diff.min(axis=0)
+    err_dict["abs_avg_err_rel"] = np.divide(abs_avg_err,
+                                            true_tec.mean(axis=0))
 
-
-    if error_type == "absolute":
-        pass
-
-    elif error_type == "relative":
-        pass
-
-    else:
-        avg_tec_err = 0
-
-    # update the current time
-
-
-
-    return avg_tec_err
-
-
-
-
+    return err_dict
 
 def main():
 
@@ -95,79 +96,61 @@ def main():
 
     stime = dt.datetime(2015, 3, 1)
     etime = dt.datetime(2015, 4, 1)
+    base_model = "previous_day"
+
+    models = ["STResNet", base_model]
+    err_types = ["Average Absolute Error"]
+
+    vmin=0; vmax=5
 
     window_len = 1 # Hour
     window_dist = 1 # Hour, skips every window_dist hours
     start_hours = [x for x in range(0, 24-window_len, window_len+window_dist)]
     end_hours = [x for x in range(window_len, 24, window_len+window_dist)]
-    
-    N = 1
 
-    # Loop through the time windows
-    for i in range(len(start_hours)): 
-        time_window = [start_hours[i], end_hours[i]]
-        aa = get_tec(stime, etime, pred_tec_dir, time_window=time_window,
-                     pred_time_step=pred_time_step, 
-                     filled_tec_dir=filled_tec_dir)
-        #avg_tec_err = calc_avg_tec()
-        N = 1
+    for model in models:
+        for err_type in err_types:
 
+            # Create empy axes
+            fig, axes = plt.subplots(nrows=3, ncols=4, figsize=(12,10),
+                                     sharex=True, sharey=True)
+            fig.subplots_adjust(hspace=0.3, wspace=0.3)
+            if len(axes) > 1:
+                axes = [ax for lst in axes for ax in lst]
+            else:
+                axes = [axes]
+            
+            # Loop through the time windows
+            for i in range(len(start_hours)): 
+            #for i in [0]: 
+                ax = axes[i]
+                time_window = [start_hours[i], end_hours[i]]
+                true_tec, pred_tec, base_tec = get_tec(stime, etime, pred_tec_dir,
+                                                       time_window=time_window,
+                                                       pred_time_step=pred_time_step,
+                                                       base_model=base_model,
+                                                       filled_tec_dir=filled_tec_dir)
+                
+                if model == "STResNet":
+                    err_dict = calc_avg_err(true_tec, pred_tec)
+                else:
+                    err_dict = calc_avg_err(true_tec, base_tec)
 
-#    #getting the datetime variables for the predicted TEC maps
-#    tec_resolution = dt.timedelta(minutes=tec_resolution)
-#    datetime_list = [stime]
-#    for _ in range(1, num_of_output_tec_maps):
-#        time = datetime_list[-1]+tec_resolution
-#        datetime_list.append(time)
-#
-#    #reading the numpy arrays
-#    pred = np.load(pred_tec_path+"_pred.npy")
-#    truth = np.load(pred_tec_path+"_true.npy")
-#
-#    #transpose for iterating over the TEC maps
-#    pred = np.transpose(pred, [2, 0, 1])
-#    truth = np.transpose(truth, [2, 0, 1])
-#
-#    for i in range(num_of_output_tec_maps):
-#        #(1, 75, 73) -> (75, 73). Making it 2D for plotting
-#        pred1 = np.squeeze(pred[i])
-#        truth1 = np.squeeze(truth[i])
-#        
-#        fig = plt.figure(figsize=(23, 13))
-#        x = list(range(225, 360, 25)) + list(range(0, 35, 15)) 
-#        y = list(range(15, 90, 10))
-#
-#        #for neglecting the missing values in the plot
-#        truth1 = np.ma.masked_where(truth1 <= -1, truth1)
-#        #setting it -0.5 (heuristic) for predicted maps
-#        pred1 = np.ma.masked_where(pred1 <= -0.5, pred1)
-#        
-#        ax1 = fig.add_subplot(231)
-#        ax2 = fig.add_subplot(233)
-#        ax3 = fig.add_subplot(234)
-#        ax4 = fig.add_subplot(235)
-#        ax5 = fig.add_subplot(236)
-#
-#        plt.suptitle(str(datetime_list[i]), fontsize=20)
-#
-#        ax1.set_xticklabels(tuple(x), fontsize=14)
-#        ax1.set_yticklabels(y, fontsize=14)
-#        ax1.set_title('Predicted TEC', fontsize=20)
-#
-#        ax2.set_xticklabels(tuple(x), fontsize=14)
-#        ax2.set_yticklabels(y, fontsize=14)
-#        ax2.set_title('True TEC', fontsize=20)
-#
-#        a = -30
-#        b= 20
-#        im1 = ax1.pcolormesh(pred1, cmap='jet', vmin=0, vmax=20)
-#        im2 = ax2.pcolormesh(truth1, cmap='jet', vmin=0, vmax=20)
-#
-#        fig.colorbar(im1, ax=ax1)
-#        fig.colorbar(im2, ax=ax2)
-#        fig.tight_layout()
-#        fig.subplots_adjust(top=0.95)
-#        fig.savefig(model_value_dir+str(i+1)+'.png', dpi=fig.dpi, bbox_inches='tight')
+                if base_tec is not None:
+                    err_dict_base = calc_avg_err(true_tec, pred_tec)
+
+                x = list(range(225, 360, 25)) + list(range(0, 35, 15)) 
+                y = list(range(15, 100, 10))
+                ax.set_xticklabels(tuple(x), fontsize=10)
+                ax.set_yticklabels(y, fontsize=10)
+                ax.set_title("UT Hour = " + str(time_window))
+
+                im = ax.pcolormesh(err_dict[err_type], cmap='jet', vmin=vmin, vmax=vmax)
+
+            fig_dir = "/home/muhammad/Dropbox/ARC/"
+            fig_name = model + "_" + "_".join(err_type.split()) + ".png"
+            fig.savefig(fig_dir+fig_name, dpi=200, bbox_inches='tight')
+
     return
 
 if __name__ == "__main__":
