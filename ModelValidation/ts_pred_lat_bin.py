3e571d0fe1993cc1b35491d912a869370a9d81d3
bharatreddy <bharatr@vt.edu>
2018-09-17 14:02:44 -0400

    ts plots

--- ModelValidation/ts_pred_lat_bin.py
+++ ModelValidation/ts_pred_lat_bin.py
@@ -41,7 +41,7 @@ class ModValTSLat(object):
         self.tecModelDict = {}
         self.tecTrueDict = {}
 
-    def read_data(self, refInpDir="/sd-data/med_filt_tec/",\
+    def read_dl_model_true_data(self, refInpDir="/sd-data/med_filt_tec/",\
                     refFileDate=datetime.datetime(2015,1,1), fType="pred"):
         """
         Read data from the npy files
@@ -126,15 +126,111 @@ class ModValTSLat(object):
                         "date").reset_index(drop=True)
         return (predTECDF, trueTECDF)
 
+    def read_baseline_model_true_data(self, refInpDir="/sd-data/med_filt_tec/",\
+                    refFileDate=datetime.datetime(2015,1,1)):
+        """
+        Read data from the npy files
+        """
+        # Get a list of all the files in the deeplearning predicted tec dir!
+        # NOTE we are reading file dates from deeplearning model
+        # since we need a 1:1 comparison. So even for the baseline
+        # model we'll only use those datetimes where we have model
+        # outputs (i.e., same time resolution).
+        fList = glob.glob( self.modelDir + "*" + "pred" + ".npy" )
+        if self.timeRange is not None:
+            # If a timerange is set generate names of all possible files
+            # We know that TEC data has a time resolution of 5 min! we'll use 
+            # that information to generate all possible file names! then use 
+            # set intersection operation to choose only those files which fall
+            # with in the time range. This I feel is a easy way to work with 
+            # variable time resolution for the predictions.
+            fNameList = []
+            currTime = self.timeRange[0]
+            while currTime <= self.timeRange[1]:
+                dtStr = currTime.strftime("%Y%m%d.%H%M")
+                fNameList.append( self.modelDir + dtStr + "_" +\
+                             "pred" + ".npy" )
+                currTime += datetime.timedelta(seconds=5*60)
+            # intersection of both the lists
+            fList = list( set(fList).intersection(set(fNameList)) )
+        # Load the actual TEC maps
+        for _fn in fList:
+            cfn = _fn.split("/")[-1]
+            fDate = cfn.split(".")[0]
+            fTime = cfn.split(".")[1].split("_")[0]
+            cd = datetime.datetime.strptime(fDate +\
+                                "-" + fTime, "%Y%m%d-%H%M")
+            pd = cd - datetime.timedelta(days=1)
+            pfDate = pd.strftime("%Y%m%d")
+            pfTime = pd.strftime("%H%M")
+            # get the fName for actual data dir
+            tfn = self.trueTecBaseDir + fDate + "/" + fDate +\
+                         "." + fTime + ".npy"
+            pfn = self.trueTecBaseDir + pfDate + "/" + pfDate +\
+                         "." + pfTime + ".npy"
+            # Load the actual data
+            self.load_npy_file(cd, pfn, "pred")
+            self.load_npy_file(cd, tfn, "true")
+        # read a dummy tec file into pandas DF to convert the numpy
+        # files into a DF with appropriate columns
+        # Read the median filtered TEC data
+        inpColList = [ "dateStr", "timeStr", "Mlat",\
+                       "Mlon", "med_tec", "dlat", "dlon" ]
+        inpRefFile = refInpDir + "tec-medFilt-" +\
+                     refFileDate.strftime("%Y%m%d") + ".txt"
+        dfRef = pandas.read_csv(inpRefFile, delim_whitespace=True,
+                         header=None, names=inpColList)
+        # Change Mlon range from 0-360 to -180 to 180
+        dfRef.loc[:, "Mlon"] = dfRef.Mlon.apply(lambda x: x if\
+                                     x<=180 else x-360)
+        mlat_min, mlon_west, mlon_east=15., -110, 34.
+        testTimeStr = 1200
+        dfRef = dfRef[ (dfRef["timeStr"] == testTimeStr) &
+            (dfRef["Mlat"] >= mlat_min) &\
+            (dfRef["Mlon"] >= mlon_west) &\
+            (dfRef["Mlon"] <= mlon_east) ].reset_index(drop=True)
+        # pivot dfRef to get the cols
+        dfRef = dfRef.pivot(index="Mlat", columns="Mlon",\
+                 values="med_tec")
+        # Predicted DF
+        predDFList = []
+        for _tdk in self.tecModelDict.keys():
+            dfRef[dfRef.columns] = self.tecModelDict[_tdk]
+            # unpivot the DF
+            ndf = dfRef.unstack().reset_index(name='med_tec')
+            # add a date col
+            ndf["date"] = _tdk
+            predDFList.append( ndf )
+        # append the DFs
+        predTECDF = pandas.concat(predDFList).sort_values(\
+                        "date").reset_index(drop=True)
+        trueDFList = []
+        for _tdk in self.tecTrueDict.keys():
+            dfRef[dfRef.columns] = self.tecTrueDict[_tdk]
+            # unpivot the DF
+            ndf = dfRef.unstack().reset_index(name='med_tec')
+            # add a date col
+            ndf["date"] = _tdk
+            trueDFList.append( ndf )
+        # append the DFs
+        trueTECDF = pandas.concat(trueDFList).sort_values(\
+                        "date").reset_index(drop=True)
+        return (predTECDF, trueTECDF)
+
 
     def generate_ts_plots(self, figName, downCastDF=True,\
-             remove_neg_tec_rows=True, legenNcols=3, lgndFontSize='x-small'):
+             remove_neg_tec_rows=True, legenNcols=4,\
+             lgndFontSize='x-small', modelType="deep",\
+              statType="median", errLatRange=[30,70]):
         """
         Generate plots based on the input conditions
         (1) remove_neg_tec_rows : remove all the rows where tec 
                                   values are negative!
         """
-        (predTECDF, trueTECDF) = self.read_data()
+        if modelType == "deep":
+            (predTECDF, trueTECDF) = self.read_dl_model_true_data()
+        else:
+            (predTECDF, trueTECDF) = self.read_baseline_model_true_data()
         # Downcast Mlon, Mlat and med_tec to float16's, this way
         # we reduce the space occupied by the DF by almost 1/2
         predTECDF["Mlon"] = predTECDF["Mlon"].astype(numpy.float16)
@@ -177,13 +273,22 @@ class ModValTSLat(object):
         trueTECDF.columns = list(colList) + ["mlat_bins"]
         # Finally get to the plotting section
         # group by mlat bins and date to develop the plot
-        mBinPredDF = predTECDF[ [ "mlat_bins", "date", "med_tec" ]\
-                     ].groupby(["mlat_bins", "date"] ).median().reset_index()
-        mBinPredDF.columns = [ "mlat_bins", "date", "mean_pred_tec" ]
-        # True DF
-        mBinTrueDF = trueTECDF[ [ "mlat_bins", "date", "med_tec" ]\
-                     ].groupby(["mlat_bins", "date"]).median().reset_index()
-        mBinTrueDF.columns = [ "mlat_bins", "date", "mean_true_tec" ]
+        if statType == "mean":
+            mBinPredDF = predTECDF[ [ "mlat_bins", "date", "med_tec" ]\
+                         ].groupby(["mlat_bins", "date"] ).mean().reset_index()
+            mBinPredDF.columns = [ "mlat_bins", "date", "mean_pred_tec" ]
+            # True DF
+            mBinTrueDF = trueTECDF[ [ "mlat_bins", "date", "med_tec" ]\
+                         ].groupby(["mlat_bins", "date"]).mean().reset_index()
+            mBinTrueDF.columns = [ "mlat_bins", "date", "mean_true_tec" ]
+        else:
+            mBinPredDF = predTECDF[ [ "mlat_bins", "date", "med_tec" ]\
+                         ].groupby(["mlat_bins", "date"] ).median().reset_index()
+            mBinPredDF.columns = [ "mlat_bins", "date", "mean_pred_tec" ]
+            # True DF
+            mBinTrueDF = trueTECDF[ [ "mlat_bins", "date", "med_tec" ]\
+                         ].groupby(["mlat_bins", "date"]).median().reset_index()
+            mBinTrueDF.columns = [ "mlat_bins", "date", "mean_true_tec" ]
         # Merge the dfs
         mBinTrueDF = pandas.merge( mBinTrueDF, mBinPredDF,\
                          on=["mlat_bins", "date"] )
@@ -193,36 +298,86 @@ class ModValTSLat(object):
                 mBinTrueDF["abs_mean_tec_err"]/mBinTrueDF["mean_true_tec"]
         # set seaborn styling
         sns.set_style("whitegrid")
-        sns.set_context("poster")
+        # sns.set_context("poster")
         # f = plt.figure(figsize=(12, 8))
         # ax = f.add_subplot(1,1,1)
-        f, axArr = plt.subplots(3, sharex=True)
+        f, axArr = plt.subplots(4, sharex=True)
         # Load an example dataset with long-form data
         # # Plot the responses for different events and regions
+        # Model predicted TEC plot
         plt1 = sns.lineplot(x="date", y="mean_pred_tec",
                      hue="mlat_bins",
                      data=mBinPredDF,ax=axArr[0])
         plt1.legend(loc=8, bbox_to_anchor=(0.5, 1.),\
                  borderaxespad=0., ncol=legenNcols, fontsize=lgndFontSize)
+        # True TEC plot
         plt2 = sns.lineplot(x="date", y="mean_true_tec",
                      hue="mlat_bins",
                      data=mBinTrueDF,ax=axArr[1])
         # remove legend from axis 'ax'
         plt2.legend_.remove()
-        plt3 = sns.lineplot(x="date", y="abs_mean_tec_err",
-                     hue="mlat_bins",
-                     data=mBinTrueDF,ax=axArr[2])
+        # Absolute error plot
+        # For this plot only include lat range
+        if errLatRange is not None:
+            mlatLabs = mBinTrueDF["mlat_bins"].cat.categories.tolist()
+            selMlatLabs = []
+            for _ml in mlatLabs:
+                if ( (_ml.left > errLatRange[0]) & (_ml.left < errLatRange[1]) ):
+                    selMlatLabs.append( _ml )
+            if len(selMlatLabs) > 0:
+                plt3 = sns.lineplot(x="date", y="abs_mean_tec_err",
+                             hue="mlat_bins",
+                             data=mBinTrueDF[ mBinTrueDF["mlat_bins"].isin(selMlatLabs)\
+                                 ],ax=axArr[2])
+            else:
+                plt3 = sns.lineplot(x="date", y="abs_mean_tec_err",
+                             hue="mlat_bins",
+                             data=mBinTrueDF,ax=axArr[2])
+        else:
+            plt3 = sns.lineplot(x="date", y="abs_mean_tec_err",
+                             hue="mlat_bins",
+                             data=mBinTrueDF,ax=axArr[2])
         # remove legend from axis 'ax'
         plt3.legend_.remove()
-        axArr[0].set_ylabel(" Pred. TEC", fontsize=14)
-        axArr[1].set_ylabel(" True. TEC", fontsize=14)
-        axArr[2].set_ylabel(" Absolute Err.", fontsize=14)
-        axArr[-1].set_xlabel("DATE (UT)", fontsize=16)
+        # Relative error plot
+        # For this plot only include lat range
+        if errLatRange is not None:
+            mlatLabs = mBinTrueDF["mlat_bins"].cat.categories.tolist()
+            selMlatLabs = []
+            for _ml in mlatLabs:
+                if ( (_ml.left > errLatRange[0]) & (_ml.left < errLatRange[1]) ):
+                    selMlatLabs.append( _ml )
+            if len(selMlatLabs) > 0:
+                plt4 = sns.lineplot(x="date", y="rel_mean_tec_err",
+                             hue="mlat_bins",
+                             data=mBinTrueDF[ mBinTrueDF["mlat_bins"].isin(selMlatLabs)\
+                                 ],ax=axArr[3])
+            else:
+                plt4 = sns.lineplot(x="date", y="rel_mean_tec_err",
+                             hue="mlat_bins",
+                             data=mBinTrueDF,ax=axArr[3])
+        else:
+            plt4 = sns.lineplot(x="date", y="rel_mean_tec_err",
+                             hue="mlat_bins",
+                             data=mBinTrueDF,ax=axArr[3])
+        # remove legend from axis 'ax'
+        plt4.legend_.remove()        
+        # label the axes
+        axArr[0].set_ylabel(" Pred. TEC", fontsize=10)
+        axArr[1].set_ylabel(" True. TEC", fontsize=10)
+        axArr[2].set_ylabel(" Abs. Err.", fontsize=10)
+        axArr[3].set_ylabel(" Rel. Err.", fontsize=10)
+        axArr[-1].set_xlabel("DATE (UT)", fontsize=10)
+        # scale error y-axis
+        axArr[2].set_ylim([0, 20.])
+        axArr[3].set_ylim([0, 1.])
         for _ax in axArr:
             _ax.get_xaxis().set_major_formatter(DateFormatter('%m-%d'))
+            _ax.tick_params(labelsize=10)
+            _ax.set_xlim( [ mBinPredDF["date"].min(), mBinPredDF["date"].max() ] )
         # Put the legend out of the figure
         # plt.legend(bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0.)
-        plt.tick_params(labelsize=16)
+        # plt.tick_params(labelsize=14)
         f.savefig(figName,bbox_inches='tight')
     
     def load_npy_file(self, currDate, fName, fType):
