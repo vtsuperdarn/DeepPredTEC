bbf9792d3a4c28e55206ad075f4a4661d66dc1ec
MuhammadVT <mamatripik@gmail.com>
2018-09-21 16:34:32 -0400

    fix path error

--- ModelValidation/plotting_loss_curves.py
+++ ModelValidation/plotting_loss_curves.py
@@ -1,10 +1,9 @@
 import seaborn as sns
 import matplotlib.pyplot as plt
 import numpy as np
+import sys
+sys.path.append("../STResNet")
 from params import Params as param
-
-#print ("Enter the model_path_values name:")
-#model_path = raw_input()
 
 param.saved_model_path = "./model_results/model_batch64_epoch100_resnet100_nresfltr6_nfltr12_of2_otec6_cf2_csl72_pf12_psl72_tf36_tsl8_gs32_ks55_exoT_nrmT_w0_yr_11_13_314.8617606163025"
 
